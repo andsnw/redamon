ccaf29fbb4c4af2b3f3430510094962211d45287
Cloudy <52116030+xdCloudy@users.noreply.github.com>
2026-09-14 18:38:23 +0100

    fix: apply Technology integrity at client close

--- graph_db/__init__.py
+++ graph_db/__init__.py
@@ -1,3 +1,9 @@
-from .neo4j_client import Neo4jClient
+from .mixins.technology_integrity_mixin import TechnologyIntegrityMixin
+from .neo4j_client import Neo4jClient as _Neo4jClient
+
+
+class Neo4jClient(TechnologyIntegrityMixin, _Neo4jClient):
+    """Public graph client with cross-scanner Technology identity integrity."""
+
 
 __all__ = ["Neo4jClient"]
